96553d123be214df67367066ad553eb60cd5afa1
Shreykrv <121611284+Shreykrv@users.noreply.github.com>
2023-08-26 13:42:10 +0530

    Update urls.py

--- portP/urls.py
+++ portP/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from portapp import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index),
+    path('', include('easypay.urls')),
+    
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
